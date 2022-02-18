0084b9eb0d339d62ed62d0d750ab1a17da08ab64
Bradley Potzka <bpotzka@gmail.com>
2022-02-17 17:33:53 -0700

    import old work

---
+++ GameCapstone/Assets/Scripts/Player/PlayerControllerJump.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerState
+{
+    NotMoving,
+    Grounded,
+    Jumping,
+    InAir,
+};
+public partial class PlayerController
+{
+    [System.Serializable]
+    public class JumpVariables
+    {
+        [HideInInspector] public float scrollWheelDelta;
+        public float jumpBuffer = .3f;
+        public float jumpStrength = 6.5f;
+        public float jumpStregthDecreaser = .05f;
+        public float jumpInAirStrength = 0;
+        public float jumpInAirControl = .1f;
+        public float jumpingInitialGravity = -.3f;
+
+        public float justJumpedCooldown = .1f;
+        public float coyoteTime = 0.15f;
+
+        public int inAirJumps = 0;
+    }
+    public void JumpInput()
+    {
+        jumpVariables.scrollWheelDelta = Input.GetAxis("Mouse ScrollWheel");
+        if (Input.GetKeyDown(KeyCode.Space) || jumpVariables.scrollWheelDelta > 0 || Input.GetButtonDown("GamePadJump") )
+        {
+            _jumpBuffer = jumpVariables.jumpBuffer;
+        }
+    }
+    public void HandleJumpInput()
+    {
+        if (_jumpBuffer <= 0) _jumpBuffer = 0;
+        if (_jumpBuffer > 0 && (isGrounded || _coyoteTimer > 0) && playerState != PlayerState.Jumping) StartCoroutine(JumpCoroutine());
+        else if (playerState == PlayerState.InAir && _inAirJumps > 0 && _jumpBuffer > 0)
+        {
+            _inAirJumps--;
+            StartCoroutine(JumpCoroutine());
+        }
+        if (_jumpBuffer > 0) _jumpBuffer -= Time.fixedDeltaTime;
+    }
+    private IEnumerator JumpCoroutine()
+    {
+        SetVariablesOnJump();
+        previousState = playerState;
+        playerState = PlayerState.Jumping;
+        y = jumpVariables.jumpStrength;
+        g = jumpVariables.jumpingInitialGravity;
+        totalVelocityToAdd += newRight;
+        airControl = jumpVariables.jumpInAirControl;
+
+        rb.velocity -= rb.velocity.y * Vector3.up;
+
+        while (rb.velocity.y >= 0f && playerState != PlayerState.Grounded)
+        {
+            y -= jumpVariables.jumpStregthDecreaser;
+            totalVelocityToAdd += Vector3.up * y;
+            yield return fixedUpdate;
+        }
+        if (playerState != PlayerState.Grounded)
+        {
+            g = 0;
+            rb.velocity -= Vector3.up * rb.velocity.y;
+            g = baseMovementVariables.initialGravity;
+        }
+        airControl = baseMovementVariables.inAirControl;
+        previousState = playerState;
+        if (!isGrounded) playerState = PlayerState.InAir;
+    }
+    public void SetVariablesOnJump()
+    {
+        _jumpBuffer = 0;
+        _justJumpedCooldown = jumpVariables.justJumpedCooldown;
+    }
+}
---
+++ GameCapstone/Assets/Scripts/Skybox/SkyboxRotator.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxRotator : MonoBehaviour
+{
+    float interpolateCount = 0.0f;
+    float start = 0.0f;
+    float end = 360.0f;
+    public float interpolationRate = 0.001f;
+    
+    // Uses the interpolation rate and Lerp to rotate the skybox material at a fixed rate.
+    // The rate was made public to allow for easy editing.
+    void FixedUpdate()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Mathf.Lerp(start, end, interpolateCount));
+        interpolateCount += interpolationRate;
+        if(RenderSettings.skybox.GetFloat("_Rotation") >= 360)
+        {
+            RenderSettings.skybox.SetFloat("_Rotation", 0);
+            interpolateCount = 0.0f;
+        }
+    }
+}
